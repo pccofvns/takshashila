693a9661b942af7f26cf50452b9ba40e0db9b38b
Prashant C Chaturvedi <pccfrmvns@gmail.com>
2023-09-26 01:37:45 +0530

    Update openapi and spring bom as import

--- docs/src/scripts/open_api_enhancer.py
+++ docs/src/scripts/open_api_enhancer.py
@@ -30,13 +30,14 @@ def enhance(properties):
     db_port = db_url_split[0]
     db_schema = db_url_split[1]
     # Load the OpenAPI YAML file
-    with open(Path(__file__).parent.parent.parent / 'modules/ROOT/assets/attachments/takshashila.yml', 'r') as yaml_file:
+    with open(Path(__file__).parent.parent.parent / 'modules/ROOT/assets/attachments/takshashila.yml',
+              'r') as yaml_file:
         openapi_spec = yaml.safe_load(yaml_file)
     # Iterate through the OpenAPI specification and update descriptions
     for schema in openapi_spec['components']['schemas'].items():
         schema_detail = schema[1]
         update_schema_description(schema_detail)
-        if (schema_detail.get('type') and schema_detail.get('properties')):
+        if schema_detail.get('type') and schema_detail.get('properties'):
             object_schema_properties = schema_detail['properties']
             for object_field in object_schema_properties:
                 update_schema_description(
@@ -47,30 +48,33 @@ def enhance(properties):
 
 
 def update_schema_description(schema_detail):
-    if (schema_detail.get('x-field-extra-annotation')):
+    if schema_detail.get('x-field-extra-annotation'):
         annotations = schema_detail['x-field-extra-annotation']
         match = re.search(pattern, annotations)
         if match:
             picklist_name = match.group(1)
             possible_values = fetch_picklist_values(picklist_name)
             if possible_values:
-                if (not schema_detail.get('example') and schema_detail.get('type') and not schema_detail['type'] == 'array'):
+                if not schema_detail.get('example') and schema_detail.get('type') and not schema_detail[
+                                                                                              'type'] == 'array':
                     schema_detail['example'] = possible_values[0]
-                if (not schema_detail.get('example') and schema_detail.get('type') and schema_detail['type'] == 'array'):
+                if not schema_detail.get('example') and schema_detail.get('type') and schema_detail['type'] == 'array':
                     schema_detail['example'] = [possible_values[0]] if len(
                         possible_values) < 2 else [possible_values[0], possible_values[1]]
                 possible_values = [append_str + sub +
                                    append_str for sub in possible_values]
-                schema_detail['description'] = f"{schema_detail.get('description', '')}. Possible values: {', '.join(possible_values)}"
+                schema_detail[
+                    'description'] = f"{schema_detail.get('description', '')}. Possible values: {', '.join(possible_values)}"
 
 
 def fetch_picklist_values(picklist_name):
     print("Processing picklist: " + picklist_name)
     with mariadb.connect(user=db_username, password=db_password,
-                          host=db_host, port=db_port, database=db_schema) as connection:
+                         host=db_host, port=db_port, database=db_schema) as connection:
         with connection.cursor() as cursor:
             values = []
-            for row in cursor.execute("SELECT NAME FROM PICKLIST WHERE PICKLIST_NAME=:pn AND LOCALE='en' ORDER BY PICKLIST_ORDER ASC", pn=picklist_name):
+            for row in cursor.execute("SELECT NAME FROM PICKLIST WHERE PICKLIST_NAME=:pn AND LOCALE='en' ORDER BY "
+                                      "PICKLIST_ORDER ASC", pn=picklist_name):
                 if row[0]:
                     values.append(row[0])
             return values
